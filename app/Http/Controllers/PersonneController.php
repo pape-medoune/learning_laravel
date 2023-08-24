000036e94ba4d19faf8e2124a1ed73d6a6efd1ba
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-08-24 14:27:23 +0000

    first step

---
+++ app/Http/Controllers/PersonneController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Models;
+
+class PersonneController extends Controller
+{
+
+
+public function index()
+{
+return ()
+}
+    // public function index()
+    // {
+    //     return view('show');
+    // }
+
+    // //Affichage
+    // public function getPersonnes()
+    // {
+    //     $members = Personne::all();
+    //     return view('show')->with(Personne, $members);
+    // }
+
+    // public function save(Request $request)
+    // {
+    //     $personne = new Personne();
+    //     $personne = $request->prenom;
+    //     $personne = $request->nom;
+    //     $personne = $request->tel;
+    //     $personne->save();
+    //     return redirect('/');
+    // }
+
+    // public function update(Request $request,$id)
+    // {
+
+    //     $personne = new Personne();
+    //     $personne = $request->prenom;
+    //     $personne = $request->nom;
+    //     $personne = $request->tel;
+    //     $personne->save();
+    //     return redirect('/');
+    // }
+}
